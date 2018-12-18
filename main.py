7581435d8c637cf061499a8c5753d8ee91019931
tsamridh86 <t.samridh.86@gmail.com>
2018-12-18 10:39:18 +0530

    added threads and able to start and stop them

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from PyQt5 import QtWidgets, QtGui, uic
 import sys
+import threading
+
+
+class Recording(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.shutdownFlag = threading.Event()
+
+    def run(self):
+        while not self.stopped():
+            print("hello")
+
+    def stop(self):
+        self.shutdownFlag.set()
+
+    def stopped(self):
+        return self.shutdownFlag.is_set()
+
 class mywindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(mywindow, self).__init__()
@@ -20,11 +38,12 @@ class mywindow(QtWidgets.QMainWindow):
 
     def startButtonClicked(self):
         print("startButtonClicked")
-        self.printInTextBox("lolwa")
+        self.recordingThread = Recording()
+        self.recordingThread.start()
 
     def stopButtonClicked(self):
         print("stopButtonClicked")
-
+        self.recordingThread.stop()
 
 app = QtWidgets.QApplication([])
 application = mywindow()
